a8cf92ec0b1519d7f8a6c5514b43d2b0913983ff
GenaroHacker <genarominetto@hotmail.com>
2023-12-11 13:32:22 +0100

    revert previous commit and change buildozer to work with preexisting files

--- utils/tasks/select_zipf_tasks.py
+++ utils/tasks/select_zipf_tasks.py
@@ -1,7 +1,6 @@
 # @title Select Five Tasks
 import sqlite3
 import random
-from utils.tasks.get_db_path import get_db_path
 
 def select_zipf_tasks():
     """
@@ -40,19 +39,8 @@ def select_zipf_tasks():
         return task_name
 
     # Create a database connection and cursor
-    # Database file name
-    db_name = 'tasks.db'
-
-    # Get the correct database path
-    db_path = get_db_path(db_name)
-
-    # Create a database connection and cursor
-    conn = sqlite3.connect(db_path)
+    conn = sqlite3.connect('tasks.db')
     cursor = conn.cursor()
-
-
-
-
 
     # Select the place marked as selected
     cursor.execute("SELECT * FROM PLACES WHERE IS_SELECTED = 1;")
