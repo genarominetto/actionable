244a4394881379fa79bb9aca79748f4717977d29
GenaroHacker <genarominetto@hotmail.com>
2023-12-13 13:23:18 +0100

    simplify all

--- screens/steps.py
+++ screens/steps.py
@@ -1,14 +1,18 @@
 from kivy.uix.screenmanager import Screen
-from utils.steps.pause import pause
-from utils.steps.cancel import cancel
-from utils.steps.next_step import next_step
 
 class stepsScreen(Screen):
     def go_to_tasks(self):
         print('Navigating from steps to tasks')
         self.manager.current = 'tasks'
+
     def on_enter(self):
         print('steps screen has fully loaded')
-    pause = staticmethod(pause)
-    cancel = staticmethod(cancel)
-    next_step = staticmethod(next_step)
+
+    def pause_or_resume(self):
+        pass
+
+    def cancel(self):
+        pass
+
+    def next_step(self):
+        pass
