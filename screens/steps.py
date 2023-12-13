0582c9e693ae167fe36eb217cd9955d8a64156c8
GenaroHacker <genarominetto@hotmail.com>
2023-12-13 17:10:32 +0100

    started working on steps screen logic

--- screens/steps.py
+++ screens/steps.py
@@ -1,18 +1,122 @@
+import sqlite3
+import random
 from kivy.uix.screenmanager import Screen
 
 class stepsScreen(Screen):
+    class Step:
+        def __init__(self, screen):
+            self.screen = screen
+            self.state = None
+            self.current_step_id, self.total_steps, self.step_name, self.order_sequence = self._get_step_info()
+            self.placeholders = self._get_placeholders()
+
+        def _get_step_info(self):
+            with sqlite3.connect("tasks.db") as conn:
+                cursor = conn.cursor()
+
+                # Fetch the latest 'Started' STEP_ID from HISTORY
+                cursor.execute("SELECT STEP_ID FROM HISTORY WHERE ACTION = 'Started' ORDER BY ID DESC LIMIT 1")
+                current_step_id = cursor.fetchone()[0]
+
+                # Fetch the details of the current step
+                cursor.execute("SELECT TASK_ID, NAME, ORDER_SEQUENCE FROM STEPS WHERE ID = ?", (current_step_id,))
+                task_id, step_name, order_sequence = cursor.fetchone()
+
+                # Calculate total steps for the task
+                cursor.execute("SELECT COUNT(ID) FROM STEPS WHERE TASK_ID = ?", (task_id,))
+                total_steps = cursor.fetchone()[0]
+
+            return current_step_id, total_steps, step_name, order_sequence
+
+        def _get_placeholders(self):
+            with sqlite3.connect("tasks.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT ID, TYPE, VALUE, RANK FROM PLACEHOLDERS")
+                return cursor.fetchall()
+
+        def select_by_zipf(self, items):
+            if not items:
+                return None
+
+            harmonic_sum = sum(1.0 / i for i in range(1, len(items) + 1))
+            probabilities = [(1.0 / i) / harmonic_sum for i in range(1, len(items) + 1)]
+            return random.choices(items, weights=probabilities, k=1)[0]
+
+        def replace_placeholders(self, step_name, placeholders):
+            while '%' in step_name:
+                start = step_name.find('%')
+                end = step_name.find('%', start + 1)
+                placeholder_type = step_name[start + 1:end]
+                relevant_placeholders = [p for p in placeholders if p[1] == placeholder_type]
+                placeholder_value = self.select_by_zipf(relevant_placeholders)[2] if relevant_placeholders else ''
+                step_name = step_name[:start] + placeholder_value + step_name[end + 1:]
+            return step_name
+
+        def change_state(self, new_state):
+            self.state = new_state
+            self._record_history()
+            self.update_ui()
+
+        def _record_history(self):
+            with sqlite3.connect("tasks.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO HISTORY (STEP_ID, ACTION, TIME) VALUES (?, ?, CURRENT_TIMESTAMP)",
+                               (self.current_step_id, self.state))
+                conn.commit()
+
+        def update_ui(self):
+            _, total_steps, step_name, order_sequence = self._get_step_info()
+            step_text = f"({order_sequence}/{total_steps}) {self.replace_placeholders(step_name, self.placeholders)}"
+
+            if self.state == "Paused":
+                self.screen.ids.pause_or_resume_button.text = "Resume"
+                self.screen.ids.next_step_button.disabled = True
+            elif self.state == "Started":
+                self.screen.ids.pause_or_resume_button.text = "Pause"
+                self.screen.ids.next_step_button.disabled = False
+            self.screen.ids.steps_completed_label.text = step_text
+            # Add logic for other states as needed
+
+        def get_next_step(self):
+            with sqlite3.connect("tasks.db") as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT ID, ORDER_SEQUENCE FROM STEPS WHERE TASK_ID = (SELECT TASK_ID FROM STEPS WHERE ID = ?) AND ORDER_SEQUENCE > ? ORDER BY ORDER_SEQUENCE ASC LIMIT 1", (self.current_step_id, self.order_sequence))
+                result = cursor.fetchone()
+            return result if result else (None, None)
+
+    def __init__(self, **kwargs):
+        super(stepsScreen, self).__init__(**kwargs)
+        self.step = self.Step(self)
+
+    def on_enter(self):
+        print('Steps screen has fully loaded')
+
     def go_to_tasks(self):
         print('Navigating from steps to tasks')
         self.manager.current = 'tasks'
 
-    def on_enter(self):
-        print('steps screen has fully loaded')
-
     def pause_or_resume(self):
-        pass
+        if self.step.state == "Paused":
+            self.step.change_state("Started")
+        else:
+            self.step.change_state("Paused")
 
     def cancel(self):
-        pass
+        self.step.change_state("Canceled")
+        self.go_to_tasks()
 
     def next_step(self):
-        pass
+        # Mark the current step as finished
+        self.step.change_state("Finished")
+
+        # Determine the next step
+        next_step_id, next_order_sequence = self.step.get_next_step()
+
+        # If there is a next step, start it
+        if next_step_id is not None:
+            self.step.current_step_id = next_step_id
+            self.step.order_sequence = next_order_sequence
+            self.step.change_state("Started")
+        else:
+            # If there are no more steps, go back to tasks screen or handle accordingly
+            self.go_to_tasks()
